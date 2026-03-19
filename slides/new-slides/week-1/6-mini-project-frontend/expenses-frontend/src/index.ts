33eab8cc06de0af06a65f5d715543b05864ad001
Gareth Nelson <gareth.nelson@kainos.com>
2026-03-19 13:41:58 +0000

    Renamed folders

--- slides/new-slides/week-1/6-mini-project-frontend/expenses-frontend/src/index.ts
+++ slides/new-slides/week-1/6-mini-project-frontend/expenses-frontend/src/index.ts
@@ -5,5 +5,5 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
